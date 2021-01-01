bdda60a96db7871798b8d656c0f97ad3cfdfa37b
Sajesh Shrestha <shresthasajs@gmail.com>
2021-01-01 14:22:32 +0545

    Fix Followers and Following bug

--- GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -16,8 +16,8 @@ class GFFollowerItemVC: GFItemInfoVC {
     }
     
     fileprivate func configureItems() {
-        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.publicRepos)
-        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.publicGists)
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
         actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
     }
     
